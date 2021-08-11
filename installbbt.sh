c3e7574f2943fa96cf1f6059a28aa3e56a68d60f
Adnan Ullah Khan <75381620+adnan-ullah-khan@users.noreply.github.com>
2021-08-11 10:59:37 +0500

    Update installbbt.sh

--- installbbt.sh
+++ installbbt.sh
@@ -10,12 +10,12 @@ cd tools
 #install knockpy
 echo "Installing knockpy"
 git clone https://github.com/guelfoweb/knock.git
-python3 $(pwd)/knock/setup.py install
+python $(pwd)/knock/setup.py install
 
 #install dirsearch
 echo "Installing dirsearch"
 git clone https://github.com/maurosoria/dirsearch.git
-python3 $(pwd)/dirsearch/setup.py install
+python $(pwd)/dirsearch/setup.py install
 
 #install smuggler
 echo "Installing smuggler"
